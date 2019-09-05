8bd88bad9ce7a774c353d1b82c45424e8b625157
Brett Jones <blockloop@users.noreply.github.com>
2019-09-05 14:39:56 -0500

    add help command

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM docker.io/mesosphere/aws-cli:1.14.5
 RUN apk add --no-cache bash inotify-tools dumb-init
 ENV S3_PATH S3_ENDPOINT LOCAL_DIR
 ADD sync.sh /app/sync.sh
+RUN chmod a+rx /app/sync.sh
 ENTRYPOINT ["/usr/bin/dumb-init", "--", "/app/sync.sh"]
+CMD ["help"]
--- sync.sh
+++ sync.sh
@@ -1,54 +1,65 @@
 #!/bin/bash
 set -ueo pipefail
 
+ME=$(basename "$0")
 S3_PATH="${S3_PATH:-s3://${AWS_BUCKET_NAME}/}"
 S3_ENDPOINT="${S3_ENDPOINT:-}"
 
 download() {
-        aws s3 sync \
-                "${S3_PATH}" \
-                "${LOCAL_DIR}" \
-                --endpoint="${S3_ENDPOINT}" \
-                --delete
+	aws s3 sync \
+		"${S3_PATH}" \
+		"${LOCAL_DIR}" \
+		--endpoint="${S3_ENDPOINT}" \
+		--delete
 
-        # fix any permissions issues
-        chmod -vR a=rwx "${LOCAL_DIR}"
+	# fix any permissions issues
+	chmod -vR a=rwx "${LOCAL_DIR}"
 }
 
 upload() {
-        aws s3 sync \
-                "${LOCAL_DIR}" \
-                "${S3_PATH}" \
-                --endpoint="${S3_ENDPOINT}" \
-                --delete
+	aws s3 sync \
+		"${LOCAL_DIR}" \
+		"${S3_PATH}" \
+		--endpoint="${S3_ENDPOINT}" \
+		--delete
 }
 
 watch_upload() {
-        inotifywait -mr "${LOCAL_DIR}" -e create -e delete -e move -e modify --format '%w%f %e' | \
-        while read -r file _ ; do
-                # ignore sqlite tmp files
-                if [[ "${file}" =~ \.db-(journal|wal|shm)$ ]]; then
-                        continue
-                fi
-                # sleeping before execution to accumulate any other file changes...
-                sleep 5
-                upload
-        done
+	inotifywait -mr "${LOCAL_DIR}" -e create -e delete -e move -e modify --format '%w%f %e' | \
+	while read -r file _ ; do
+		# ignore sqlite tmp files
+		if [[ "${file}" =~ \.db-(journal|wal|shm)$ ]]; then
+			continue
+		fi
+		# sleeping before execution to accumulate any other file changes...
+		sleep 5
+		upload
+	done
 }
 
 main() {
-        case "${1}" in
-                download)
-                        download
-                        ;;
-                upload)
-                        watch_upload
-                        ;;
-                *)
-                        echo "$(date -u): Unknown command: ${1}" > /dev/stderr
-                        exit 1
-                        ;;
-        esac
+	case "${1}" in
+		download)
+			download
+			;;
+		upload)
+			watch_upload
+			;;
+		help|--help|-h)
+			cat <<-EOF
+			Usage: ${ME} [command]
+
+			Commands:
+			  upload - watch for changes in LOCAL_DIR and sync LOCAL_DIR to S3
+			  download - download all remote files to LOCAL_DIR and exit
+			EOF
+			exit 0
+			;;
+		*)
+			echo "$(date -u): Unknown command: ${1}" > /dev/stderr
+			exit 1
+			;;
+	esac
 }
 
 
